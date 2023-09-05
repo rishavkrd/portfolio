5a6d8022321e99f53704873415fa2e7f32783017
rishavkrd <rishavkrd11@tamu.edu>
2023-09-05 14:31:37 -0500

    add react portfolio

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static(publicDirPath));
 app.get('/', function (req, res) {
   res.sendFile( __dirname + "/" + "index.html" );
 });
+app.get('/portfolio', function (req, res) {
+  res.sendFile( __dirname + "/" + "react_portfolio.html" );
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
